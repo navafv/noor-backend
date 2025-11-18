8ec4b27371ee6f4459f5a6c0858d265df41111bc
Navaf V <navafkcp@gmail.com>
2025-11-18 23:01:17 +0530

    implementing

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
+# 1. Import load_dotenv
+from dotenv import load_dotenv 
+
+# 2. Load the .env file
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
 
