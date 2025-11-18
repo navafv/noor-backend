2db8853162b1d86692af22f563c4979183c3c151
Navaf V <navafkcp@gmail.com>
2025-11-18 23:26:56 +0530

    implementing

--- core/settings/base.py
+++ core/settings/base.py
@@ -83,7 +83,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "frontend_build"]
+STATICFILES_DIRS = []
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
